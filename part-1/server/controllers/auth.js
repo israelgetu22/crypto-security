49a8b4e41beb7f268cacfc7c9a2acb7434a0a63d
Isarel Getu <israelgetu101@gmail.com>
2022-03-03 17:29:49 -0600

    Well Done

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -1,21 +1,51 @@
-const users = []
+const bcryptjs = require("bcryptjs");
+
+const users = [];
 
 module.exports = {
-    login: (req, res) => {
-      console.log('Logging In User')
-      console.log(req.body)
-      const { username, password } = req.body
-      for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
+  login: (req, res) => {
+    console.log("Logging In User");
+    console.log(req.body);
+    const { username, password } = req.body;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].username === username) {
+        const authenicated = bcryptjs.compareSync(
+          password,
+          users[i].hashedPassword
+        );
+
+        if (authenicated) {
+          let savedUser = { ...users[i] };
+          delete savedUser.hashedPassword;
+          res.status(200).send(savedUser);
         }
+        //res.status(200).send(users[i]);
       }
-      res.status(400).send("User not found.")
-    },
-    register: (req, res) => {
-        console.log('Registering User')
-        console.log(req.body)
-        users.push(req.body)
-        res.status(200).send(req.body)
     }
-}
+    res.status(400).send("User not found.");
+  },
+  register: (req, res) => {
+    const { username, email, firstName, lastName, password } = req.body;
+
+    const salt = bcryptjs.genSaltSync(7);
+    const hashedPassword = bcryptjs.hashSync(password, salt);
+
+    let user = {
+      username,
+      email,
+      firstName,
+      lastName,
+      hashedPassword,
+    };
+
+    users.push(user);
+    let savedUser = { ...user };
+    delete savedUser.hashedPassword;
+    res.status(200).send(savedUser);
+
+    // console.log("Registering User");
+    // console.log(req.body);
+    // users.push(req.body);
+    // res.status(200).send(req.body);
+  },
+};
